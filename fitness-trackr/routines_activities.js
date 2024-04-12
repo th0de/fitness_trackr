47fdc85cb9e0f6148f2c5ac586c369dd7d171d6d
Elijah Thode <elijahthode@gmail.com>
2024-04-11 22:54:03 -0500

    trying new format for routines_activities function

--- fitness-trackr/routines_activities.js
+++ fitness-trackr/routines_activities.js
@@ -1,94 +1,18 @@
-const { createActivities } = require('./activities.js');
-const { routinesActivities } = require('./routines_activities.js');
+const client = require('./client.cjs');
 
-const routinesActivities = async() => { 
-  try{
-    try{
-      await client.query(`
-      INSERT INTO activities (name)
-      VALUES ('${createActivities}')
-      INSERT INTO activities (id)
-      VALUES ('${createActivities}')
-      INSERT INTO activities (description)
-      VALUES ('${createActivities}) 
-      `); 
- 
-    }catch (error) {
-    console.log(error);
-    }
-  
-  
-  const getActivities = async () => {
-    try{ 
-      await client.query(`
-      SELECT id FROM activities
-      SELECT name FROM activities
-      SELECT`)
-    } catch(error) {
-    console.log(error);
-    }
-  
-
-  try{
+const routinesActivities = async(routines_id, activities_id) => {
+  try {
     await client.query(`
-    INSERT INTO routines (id)
-    VALUES ('${createRoutines}')
-    INSERT INTO routines (is_public)
-    VALUES ('${createRoutines}')
-    INSERT INTO routines (name)
-    VALUES ('${createRoutines})
-    INSERT INTO routines (goal) `);
-    }
-  catch (error) 
-  {
-  console.log(error);
-  }
-  
-  
-  try{
-    
-    await client.query(`
-    SELECT id FROM routines
-    SELECT is_public FROM routines
-    SELECT name FROM routines
-    SELECT goal FROM routines
-    `)
-    } catch (error) {
-    console.log(error);
-    } 
-  }
-  
-  
-  
-  try{
-    await client.query(`
-    INSERT INTO routines (id)
-    VALUES ('${createRoutines}')
-    INSERT INTO routines (is_public)
-    VALUES ('${createRoutines}')
-    INSERT INTO routines (name)
-    VALUES ('${createRoutines})
-    INSERT INTO routines (goal) `);
-    }catch (error) {
-  console.log(error);
-  }
-  
-  try{
-    
-    await client.query(`
-    SELECT id FROM routines
-    SELECT is_public FROM routines
-    SELECT name FROM routines
-    SELECT goal FROM routines
-    `)
-    } catch (error) {
-    console.log(error);
-    } 
-  }catch (error){
-    console.log(error);
+      INSERT INTO routinesActivities
+      (routines_id, activities_id)
+      VALUES 
+      ('${routines_id}', '${activities_id}');
+     `);
+   } catch(err) {
+    console.log(err);   
   }
 }
-// a routines_activities table with an id, routine_id, activity_id, count
+
 
 module.exports = {
   routinesActivities
